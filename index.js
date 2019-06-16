5ec46aebc4eccd3452857a7fa1978329d82720cd
HenriqueMaga <henrique.ma400@gmail.com>
2019-06-16 12:34:41 -0300

    Formulário preenche variável no Submit

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 let campos = document.querySelectorAll('#form-user-create [name]');
+let dados = {};
 
-campos.forEach((campo,index)=>{
-    if(campo.name == 'gender'){
-        if(campo.checked){
-            console.log(campo.name, campo.value);
+document.getElementById('form-user-create').addEventListener('submit', e=>{
+    e.preventDefault();
+    campos.forEach((campo, index)=>{
+        if(campo.name == 'gender'){
+            if(campo.checked){
+                dados[campo.name] = campo.value;
+            }
         }
-    }
-    else{
-        console.log(campo.name, campo.value);
-    }
+        else{
+            dados[campo.name] = campo.value;
+        }
+    });
+    console.log(dados);
 });
