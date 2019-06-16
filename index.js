73bb040ba5402e282bebff0de527356d321c771c
HenriqueMaga <henrique.ma400@gmail.com>
2019-06-16 12:52:22 -0300

    Usuários criados são exibidos na tela com innerHTML no Submit

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 let campos = document.querySelectorAll('#form-user-create [name]');
 let dados = {};
 
+function adicionarLinhaDeUsuario(){
+    let tr = document.createElement('tr');
+    
+    tr.innerHTML = `
+    <tr>
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${dados.name}</td>
+        <td>${dados.email}</td>
+        <td>${dados.admin}</td>
+        <td>${dados.birth}</td>
+        <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+    </tr>
+    `
+    document.getElementById('table-user').appendChild(tr);
+}
+
 document.getElementById('form-user-create').addEventListener('submit', e=>{
     e.preventDefault();
     campos.forEach((campo, index)=>{
@@ -13,5 +32,6 @@ document.getElementById('form-user-create').addEventListener('submit', e=>{
             dados[campo.name] = campo.value;
         }
     });
+    this.adicionarLinhaDeUsuario();
     console.log(dados);
 });
